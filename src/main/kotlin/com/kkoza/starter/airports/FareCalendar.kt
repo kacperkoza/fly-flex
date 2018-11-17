301fdc648117947fcd96ae4b54f3b8f6aac7c252
Kamil Pabin <pabin.kamil@gmail.com>
2018-11-18 00:07:22 +0100

    Adds Calendar Client

--- src/main/kotlin/com/kkoza/starter/airports/FareCalendar.kt
+++ src/main/kotlin/com/kkoza/starter/airports/FareCalendar.kt
@@ -3,12 +3,12 @@ package com.kkoza.starter.airports
 import org.joda.time.DateTime
 
 data class FareCalendar(
-        val departureAirport: Airport,
-        val arrivalAirport: Airport,
+        val departureAirportIata: String,
+        val arrivalAirportIata: String,
         val fares: List<FlightInfo>
 )
 
 data class FlightInfo(
         val date: DateTime,
-        val price: Int
+        val price: Int?
 )
---
+++ src/main/kotlin/com/kkoza/starter/airports/infrastructure/client/ryanair/RyanAirFareCalendarClient.kt
@@ -0,0 +1,55 @@
+package com.kkoza.starter.airports.infrastructure.client.ryanair
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.kkoza.starter.airports.FareCalendar
+import com.kkoza.starter.airports.FareCalendarClient
+import com.kkoza.starter.airports.FlightInfo
+import org.joda.time.DateTime
+import reactor.core.publisher.Mono
+
+class RyanAirFareCalendarClient(
+        private val ryanAirClient: RyanAirClient,
+        private val url: String
+) : FareCalendarClient {
+    private val path = "$url/farefinder/3/oneWayFares"
+
+    companion object {
+        private const val BASE_PARAMS = "/cheapestPerDay?market=pl-pl&outboundMonthOfDate="
+    }
+
+    override fun getFareCalendar(departureIata: String, arrivalIata: String): Mono<FareCalendar> {
+        val finalPath = "$path/$departureIata/$arrivalIata${BASE_PARAMS}2018-11-01"
+        return ryanAirClient
+                .requestMono(finalPath,
+                        ResponseDto::class.java)
+                { calendarDto: ResponseDto -> mapToFareCalendar(departureIata, arrivalIata, calendarDto) }
+    }
+
+    private fun mapToFareCalendar(departureIata: String, arrivalIata: String,
+                                  calendarDto: ResponseDto): Mono<FareCalendar> {
+        val flightInfos = calendarDto.outbound.fares
+                .map { FlightInfo(it.day, it.price?.valueMainUnit?.toInt()) }
+
+        return Mono.just(FareCalendar(departureIata, arrivalIata, flightInfos))
+    }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ResponseDto(
+        val outbound: OutboundDto
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class OutboundDto(val fares: List<FareDto>)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class FareDto(
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        val day: DateTime,
+        val price: PriceDto?)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class PriceDto(
+        val valueMainUnit: String?
+)
