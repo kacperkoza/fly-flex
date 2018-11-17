0368c1222006af44d2e2fa76199deac0018d0c07
Kamil Pabin <pabin.kamil@gmail.com>
2018-11-17 14:08:03 +0100

    Fixes tests

--- src/test/kotlin/com/kkoza/starter/BaseIntegrationTest.groovy
+++ src/test/kotlin/com/kkoza/starter/BaseIntegrationTest.groovy
@@ -2,7 +2,6 @@ package com.kkoza.starter
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -15,8 +14,4 @@ class BaseIntegrationTest extends Specification {
 
     @Autowired
     WebTestClient webTestClient
-
-    @Autowired
-    ReactiveMongoTemplate reactiveMongoTemplate
-
 }
